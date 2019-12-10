5d6e963f4dfb8cd76460707f343875b0adc5118d
abdellah.aitziad <abdellah.aitizad@berexia.com>
2019-12-10 10:58:56 +0100

    First commit

--- src/main/java/com/stud/jhp/config/SecurityConfiguration.java
+++ src/main/java/com/stud/jhp/config/SecurityConfiguration.java
@@ -1,10 +1,8 @@
 package com.stud.jhp.config;
 
-import com.stud.jhp.security.*;
-import com.stud.jhp.security.jwt.*;
-
-import org.springframework.beans.factory.BeanInitializationException;
-import org.springframework.beans.factory.InitializingBean;
+import com.stud.jhp.security.AuthoritiesConstants;
+import com.stud.jhp.security.jwt.JWTConfigurer;
+import com.stud.jhp.security.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
@@ -63,24 +61,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .exceptionHandling()
             .authenticationEntryPoint(problemSupport)
             .accessDeniedHandler(problemSupport)
-        .and()
+            .and()
             .headers()
             .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:")
-        .and()
+            .and()
             .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
-        .and()
+            .and()
             .featurePolicy("geolocation 'none'; midi 'none'; sync-xhr 'none'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; speaker 'none'; fullscreen 'self'; payment 'none'")
-        .and()
+            .and()
             .frameOptions()
             .deny()
-        .and()
+            .and()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
+            .and()
             .authorizeRequests()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
+            .antMatchers("/h2-console").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/**").authenticated()
@@ -88,9 +87,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/prometheus").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
-        .and()
+            .and()
             .httpBasic()
-        .and()
+            .and()
             .apply(securityConfigurerAdapter());
         // @formatter:on
     }
--- src/main/java/com/stud/jhp/security/UserNotActivatedException.java
+++ src/main/java/com/stud/jhp/security/UserNotActivatedException.java
@@ -5,7 +5,7 @@ import org.springframework.security.core.AuthenticationException;
 /**
  * This exception is thrown in case of a not activated user trying to authenticate.
  */
-public class UserNotActivatedException extends AuthenticationException {
+public class    UserNotActivatedException extends AuthenticationException {
 
     private static final long serialVersionUID = 1L;
 
